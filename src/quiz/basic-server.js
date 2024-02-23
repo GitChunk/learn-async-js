a60b3437a93015214b9925e844674c43d1292dfa
GitChunk <121046276+GitChunk@users.noreply.github.com>
2024-02-22 20:01:55 -0500

    finish in class work

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -45,11 +45,15 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
             })));
+        } else {
+            // If the request is for an unsupported endpoint, send a not found response
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Missing parameters. Please provide road, location, direction, and id.');
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
         res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Endpoint not found');
+        res.end('Missing parameters. Please provide road, location, direction, and id.');
     }
 });
 
